14fb4d6212eedc7373cb8c6d1fc6ff759727308b
ianwal <52143079+ianwal@users.noreply.github.com>
2024-07-25 17:48:01 -0700

    wip add frame hasher

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,37 @@
 #include <pybind11/pybind11.h>
 
 #include <pdq/cpp/common/pdqhashtypes.h>
+#include <pdq/cpp/hashing/bufferhasher.h>
 
-int add(int i, int j) {
+namespace py = pybind11;
+
+int add(int i, int j)
+{
     return i + j;
 }
 
-int hamming_distance(std::string const& a, std::string const& b) {
+int hamming_distance(std::string const &a, std::string const &b)
+{
     facebook::pdq::hashing::Hash256 hash_a{a.c_str()};
     facebook::pdq::hashing::Hash256 hash_b{b.c_str()};
     return hash_a.hammingDistance(hash_b);
 }
 
-PYBIND11_MODULE(stuff, m) {
-    m.doc() = "hvdaccelerators plugin to make stuff fast"; // optional module docstring
+py::bytes hash_frame(py::bytes &img, size_t width, size_t height)
+{
+    auto hasher = facebook::vpdq::hashing::FrameBufferHasherFactory::createFrameHasher(width, height);
+    facebook::pdq::hashing::Hash256 result{};
+    int quality{};
+    std::string img_str{img};
+    hasher->hashFrame(reinterpret_cast<unsigned char *>(img_str.data()), result, quality);
+    return result.format();
+}
+
+PYBIND11_MODULE(stuff, m)
+{
+    m.doc() = "hvdaccelerators plugin to make stuff fast";
 
     m.def("add", &add, "A function that adds two numbers");
     m.def("hamming_distance", &hamming_distance, "A function that adds two numbers");
+    m.def("hash_frame", &hash_frame, "hash a frame");
 }
--- tests/test_smoketest.py
+++ tests/test_smoketest.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import logging
 import unittest
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 from hvdaccelerators import stuff
@@ -27,6 +28,21 @@ class TestSmokeTest(unittest.TestCase):
             "0000000000000000000000000000000000000000000000000000000000000000",
         )
         self.assertEqual(result, 63)
+
+    def test_hashing(self):
+        from io import BytesIO
+
+        from PIL import Image
+
+        with open(Path(__file__).parent / "test.jpg", "rb") as f:
+            data = f.read()
+            im = Image.open(BytesIO(data))
+            im.thumbnail((512, 512))
+            im.convert("RGB")
+            result = stuff.hash_frame(im.tobytes(), im.width, im.height)
+            pdq_hash = str(result, encoding="utf-8")
+            self.assertEqual(pdq_hash, "1234")
+            print(result)
 
 
 if __name__ == "__main__":
